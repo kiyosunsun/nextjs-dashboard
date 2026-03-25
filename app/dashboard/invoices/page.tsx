ff1f0f959f7b67a97bae538bec4698227a41bbcb
Setsuna <601890383@qq.com>
2026-03-25 17:33:07 +0800

    Add dashboard and fonts

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+/*
+ * @Author: Setsuna
+ * @Email: setsuna.xyz@qq.com
+ * @FilePath: \nextjs-dashboard\app\dashboard\invoices\page.tsx
+ * @Date: 2026-03-23 15:37:13
+ * @LastEditTime: 2026-03-23 15:37:27
+ * @LastEditors: Setsuna
+ */
+export default function Page() {
+  return <p>Invoices Page</p>;
+}
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: Setsuna
+ * @Email: setsuna.xyz@qq.com
+ * @FilePath: \nextjs-dashboard\app\dashboard\layout.tsx
+ * @Date: 2026-03-23 15:37:53
+ * @LastEditTime: 2026-03-23 15:37:53
+ * @LastEditors: Setsuna
+ */
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}

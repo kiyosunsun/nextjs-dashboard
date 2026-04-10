3c5d30216335f039f287266e0f48e7e59410a483
Setsuna <601890383@qq.com>
2026-04-10 17:32:10 +0800

    ``` feat(dashboard): 新增发票编辑页面
    新增 app/dashboard/invoices/[id]/edit/page.tsx 文件，
    创建发票编辑功能的基础页面结构。
    ```

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,8 @@
+/*
+ * @Author: Setsuna
+ * @Email: setsuna.xyz@qq.com
+ * @FilePath: \nextjs-dashboard\app\dashboard\invoices\[id]\edit\page.tsx
+ * @Date: 2026-04-10 17:18:33
+ * @LastEditTime: 2026-04-10 17:18:49
+ * @LastEditors: Setsuna
+ */
